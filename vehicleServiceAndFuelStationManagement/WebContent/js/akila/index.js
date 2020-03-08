49772f58efbe163ed3719e5f95c599007ac723f0
Akila Liyanage <mlakilaliyanage@gmail.com>
2020-03-08 11:28:49 +0530

    sign in page animations

--- vehicleServiceAndFuelStationManagement/WebContent/js/akila/index.js
+++ vehicleServiceAndFuelStationManagement/WebContent/js/akila/index.js
@@ -1,1 +1,6 @@
-const sighUpButton = document.getElementById(signUp);
+const sighUpButton = document.getElementById("signUp");
+const sighInButton = document.getElementById("signIn");
+const container = document.getElementById("back-container");
+
+sighUpButton.addEventListener('click',() => container.classList.add('right-panel-active'));
+sighInButton.addEventListener('click',() => container.classList.remove('right-panel-active'));
